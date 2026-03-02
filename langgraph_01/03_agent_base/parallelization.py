65b8bbb4bfc3dce58ff70d52875484985a26756b
lshmbc52 <lshmbc52@gmail.com>
2026-03-02 15:26:12 +0900

    Daily update: 2026-03-02 15:26

--- langgraph_01/03_agent_base/parallelization.py
+++ langgraph_01/03_agent_base/parallelization.py
@@ -1,38 +1,41 @@
 from langchain.chat_models import init_chat_model
 from typing import TypedDict
-from langgraph.graph import StateGraph, START,END
+from langgraph.graph import StateGraph, START, END
 
-model = init_chat_model(model='solar-pro',
-                        model_provider='upstage',
-                        temperature= 1.0)
+model = init_chat_model(model="solar-pro", model_provider="upstage", temperature=1.0)
+
 
 class WriteState(TypedDict):
-    topic:str
-    poem:str
-    story:str
-    joke:str
-    final_report:str
+    topic: str
+    poem: str
+    story: str
+    joke: str
+    final_report: str
 
-def write_poem(state:WriteState):
+
+def write_poem(state: WriteState):
     topic = state["topic"]
-    print(f"[work AI] '{topic}' 주제로 시(Poem)작성 시작...")
+    print(f"[worker A ] '{topic}' 주제로 시(Poem)작성 시작...")
 
     msg = model.invoke(f" '{topic}'에 대한 서정적인 시를 짧게  써줘")
     return {"poem": msg.content}
 
-def write_story(state:WriteState):
-    state = state['topic']
-    print(f"[workAI] {'topic'}를 주제로 소설(story)작성 시작")
-    msg = model.invoke(f"[workAI] {'topic'}를 주제로 짧고 감동적인 소설을 작성해 줘")
-    return{"story":msg.content}
 
-def write_joke(state:WriteState):
-    topic= state['topic']
-    print(f'[work AI] 조크(joke)작성 시작')
+def write_story(state: WriteState):
+    topic = state["topic"]
+    print(f"[worker B] '{topic}' 주제로 소설(story)작성 시작")
+    msg = model.invoke(f"'{topic}'를 주제로 짧고 감동적인 소설을 작성해 줘")
+    return {"story": msg.content}
+
+
+def write_joke(state: WriteState):
+    topic = state["topic"]
+    print(f"[worker C] 조크(joke)작성 시작")
     msg = model.invoke(f"{topic}을 주제로 재미있는 조크를 짧게 만들어 줘")
-    return {"joke":msg.content}
+    return {"joke": msg.content}
 
-def aggregator(state:WriteState):
+
+def aggregator(state: WriteState):
     print(f"\n-- [aggregator]모든원고 도착 최종편집중---")
     final_text = f"""
     1.시(Poem)
@@ -46,7 +49,8 @@ def aggregator(state:WriteState):
     ________________
     {state['joke']}
     """
-    return{"final_report": final_text}
+    return {"final_report": final_text}
+
 
 workflow = StateGraph(WriteState)
 
@@ -56,24 +60,26 @@ workflow.add_node("write_joke", write_joke)
 workflow.add_node("aggregator", aggregator)
 
 
-workflow.add_edge(START,'write_poem')
-workflow.add_edge(START,'write_story')
-workflow.add_edge(START,'write_joke')
+workflow.add_edge(START, "write_poem")
+workflow.add_edge(START, "write_story")
+workflow.add_edge(START, "write_joke")
 
-workflow.add_edge('write_poem','aggregator')
-workflow.add_edge('write_story','aggregator')
-workflow.add_edge('write_joke', 'aggregator')
-workflow.add_edge('aggregator', END)
+workflow.add_edge("write_poem", "aggregator")
+workflow.add_edge("write_story", "aggregator")
+workflow.add_edge("write_joke", "aggregator")
+workflow.add_edge("aggregator", END)
 
-app =  workflow.compile()
+app = workflow.compile()
 
 from IPython.display import Image
 
 app.get_graph().draw_mermaid_png(output_file_path="graph_image_para.png")
 print("그래프 저장완료")
 
-inputs = {"topic":"젊은 날의 봄"}
-result = app.invoke(inputs)
-print(result)
+if __name__ == "__main__":
 
-print(result["final_report"])
+    inputs = {"topic": "어느 70대 중반의 봄의 의미는?"}
+    result = app.invoke(inputs)
+    print(result)
+
+    print(result["final_report"])
--- langgraph_01/03_agent_base/prompt_chaining.py
+++ langgraph_01/03_agent_base/prompt_chaining.py
@@ -1,79 +1,83 @@
 from langchain.chat_models import init_chat_model
 from typing import TypedDict
-from langgraph.graph import StateGraph, START,END
+from langgraph.graph import StateGraph, START, END
 import common_utils as utils
 
-model = utils.get_solar_model(model_name="solar-pro",temperature=1.0)
+model = utils.get_solar_model(model_name="solar-pro", temperature=1.0)
 
-#model = init_chat_model("solar-pro", temperature=1.0)
+# model = init_chat_model("solar-pro", temperature=1.0)
+
 
 class MovieState(TypedDict):
-    topic:str
-    draft_comment:str
-    improved_comment:str
-    final_comment:str
+    topic: str
+    draft_comment: str
+    improved_comment: str
+    final_comment: str
 
-def generate_movie_comment(state:MovieState):
+
+def generate_movie_comment(state: MovieState):
     topic = state["topic"]
     print(f"\n---[1단계] '{topic}'주제로  영화 코멘트 초안 생성중 ")
-    msg = model.invoke(f"'{topic}'에 대한 짧은 코멘트를 만들어주고. 다른 말은 하지말고 코멘트 하나면 돼")
-    return {'draft_comment':msg.content}
+    msg = model.invoke(
+        f"'{topic}'에 대한 짧은 코멘트를 만들어주고. 다른 말은 하지말고 코멘트 하나면 돼"
+    )
+    return {"draft_comment": msg.content}
 
-def critique_and_improve(state:MovieState):
+
+def critique_and_improve(state: MovieState):
     original_comment = state["draft_comment"]
     print(f"\n --[2단계] 좀 더 깊게 수정중")
-    
+
     prompt = f"""
             다음 코멘트를 보고, 더 깊이있는 영화코멘트로 개선해주고, 다른 말은 하지말고 하나만 답변해 줘
             원문:{original_comment}
             """
     msg = model.invoke(prompt)
-    return{'improved_comment':msg.content}
+    return {"improved_comment": msg.content}
 
-def polish_comment(state:MovieState):
+
+def polish_comment(state: MovieState):
     improved_comment = state["improved_comment"]
     print(f"\n---- [3단계] 이모지 추가및 마무리")
-    prompt =f"""
+    prompt = f"""
             다음 영화에 적절한 이모지를 듬뿍 넣어서 SNS에 올리기 좋게 꾸며 줘.
             농담:{improved_comment}
             """
     msg = model.invoke(prompt)
-    return{"final_comment":msg.content}
+    return {"final_comment": msg.content}
+
 
 app = StateGraph(MovieState)
-app.add_node('generate_movie_comment',generate_movie_comment)
-app.add_node('critique_and_improve',critique_and_improve)
-app.add_node('polish_comment',polish_comment)
+app.add_node("generate_movie_comment", generate_movie_comment)
+app.add_node("critique_and_improve", critique_and_improve)
+app.add_node("polish_comment", polish_comment)
 
-app.add_edge(START,'generate_movie_comment')
-app.add_edge('generate_movie_comment','critique_and_improve')
-app.add_edge('critique_and_improve','polish_comment')
-app.add_edge('polish_comment',END)
+app.add_edge(START, "generate_movie_comment")
+app.add_edge("generate_movie_comment", "critique_and_improve")
+app.add_edge("critique_and_improve", "polish_comment")
+app.add_edge("polish_comment", END)
 
-chain= app.compile()
+chain = app.compile()
 from IPython.display import Image
 
 chain.get_graph().draw_mermaid_png(output_file_path="graph_image_movie.png")
 print("그래프 저장완료")
 
-inputs = {"topic":"그을린 사랑(Incendies)"}
-# result = chain.invoke(inputs)
-# print(result)
-try:
-    print("--- 통신 시도 중... ---")
-    for chunk in chain.stream(inputs):
-        print(f"현재 단계: {list(chunk.keys())[0]}")
-except Exception as e:
-    print(f"에러 발생: {e}")
+if __name__ == "__main__":
 
+    inputs = {"topic": "그을린 사랑(Incendies)"}
+    result = chain.invoke(inputs)
+    print(result)
 
+    print(f"주제:{result['topic']}")
+    print(f"1차초안:{result['draft_comment']}")
+    print(f"2차초안:{result['improved_comment']}")
+    print("------------------")
+    print(f"최종완성:\n{result['final_comment']}")
 
-
-
-
-
-
-
-
-
-
+    # try:
+    #     print("--- 통신 시도 중... ---")
+    #     for chunk in chain.stream(inputs):
+    #         print(f"현재 단계: {list(chunk.keys())[0]}")
+    # except Exception as e:
+    #     print(f"에러 발생: {e}")
